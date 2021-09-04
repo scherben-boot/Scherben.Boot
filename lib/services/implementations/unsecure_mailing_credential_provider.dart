2efdfbfa229be0afc50c3e0193861fdf73006f58
Luca Schmider <luca.schmider@outlook.com>
2021-09-04 10:26:34 +0200

    implemented unsecure credential provider

---
+++ lib/services/implementations/unsecure_mailing_credential_provider.dart
@@ -0,0 +1,10 @@
+import 'package:scherben_boot/models/mailing_credentials.dart';
+import 'package:scherben_boot/services/mailing_credential_provider.service.dart';
+
+class UnsecureMailingCredentialProvider implements MailingCredentialProvider {
+  @override
+  Future<MailingCredentials> getCredentials() {
+    return Future.value(MailingCredentials(
+        "scherben.boot@gmail.com", "gBSmAKXcVV\$g:E\"(\$lCZ"));
+  }
+}
