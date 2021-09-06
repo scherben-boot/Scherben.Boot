e9ad29d2ecce5887846f0eba7d88c74a3e88e214
Luca Schmider <luca.schmider@outlook.com>
2021-09-06 12:25:31 +0200

    updated reporting button

--- lib/views/components/reporting_button.dart
+++ lib/views/components/reporting_button.dart
@@ -14,55 +14,57 @@ class ReportingButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ElevatedButton(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              padding: const EdgeInsets.only(left: 15.0, top: 15.0),
-              alignment: Alignment.topLeft,
-              child: Text(
-                metadata.buttonDescription,
-                style: new TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
-                alignment: Alignment.bottomRight,
-                child: Icon(
-                  metadata.buttonIcon,
-                  color: Colors.grey[400],
-                  size: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(left: 15.0, top: 15.0),
+                alignment: Alignment.topLeft,
+                child: Text(
+                  metadata.buttonDescription,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontFamily: "roboto",
+                  ),
+                  textAlign: TextAlign.left,
                 ),
               ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Icon(
+                    metadata.buttonIcon,
+                    size: 85,
+                    color: Colors.grey[400],
+                  ),
+                ),
+              )
+            ],
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ReportingView(metadata: metadata)),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            primary: metadata.primaryColor,
+            onSurface: Colors.orange,
+            elevation: 5.5,
+            padding: const EdgeInsets.only(
+              top: 4.0,
+              bottom: 4.0,
+              right: 8.0,
+              left: 8.0,
             ),
-          ],
-        ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ReportingView(metadata: metadata)),
-          );
-        },
-        style: ElevatedButton.styleFrom(
-          primary: metadata.primaryColor,
-          onPrimary: Colors.white,
-          onSurface: Colors.orange,
-          shadowColor: Colors.black.withOpacity(0.9),
-          elevation: 10.0,
-          textStyle: TextStyle(fontFamily: 'roboto'),
-          padding: const EdgeInsets.only(
-              top: 4.0, bottom: 4.0, right: 8.0, left: 8.0),
-          minimumSize: Size(MediaQuery.of(context).size.width * 0.8, 0),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
         ),
       ),
     );
